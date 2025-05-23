135c65db2cd8ca510b038cfd4b27fe4c9363f7c9
Avalanche7CZ <avalanche752@gmail.com>
2025-05-23 22:26:35 +0200

    Refactor: Improve chunk loader management and logging
    Improve chunk loader management by using a BukkitRunnable for
    player login/logout events, enhancing efficiency and
    reducing potential conflicts.  Add more detailed logging
    for debugging and monitoring purposes.  Improve error
    handling and messaging for a better user experience.
    
    (main)

--- src/main/java/eu/avalanche7/BetterChunkLoader.java
+++ src/main/java/eu/avalanche7/BetterChunkLoader.java
@@ -12,7 +12,6 @@ import net.kaikk.mc.bcl.forgelib.BCLForgeLib;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BetterChunkLoader extends JavaPlugin {
@@ -20,23 +19,26 @@ public class BetterChunkLoader extends JavaPlugin {
 
 	private Config config;
 
+	@Override
 	public void onLoad() {
 		instance = this;
 		DataStoreManager.registerDataStore("XML", XmlDataStore.class);
 		DataStoreManager.registerDataStore("MySQL", MySqlDataStore.class);
 		DataStoreManager.registerDataStore("MariaDB", MariaDBDataStore.class);
-
 	}
 
+	@Override
 	public void onEnable() {
 		try {
 			Class.forName("net.minecraftforge.common.ForgeVersion");
 		} catch (ClassNotFoundException e) {
+			getLogger().severe("Thermos/KCauldron/Crucible (or a compatible Forge environment) is NOT detected!");
 			throw new RuntimeException("Thermos/KCauldron/Crucible and BCLForgeLib are needed to run this plugin!");
 		}
 		try {
 			Class.forName("net.kaikk.mc.bcl.forgelib.BCLForgeLib");
 		} catch (ClassNotFoundException e) {
+			getLogger().severe("BCLForgeLib mod is NOT detected!");
 			throw new RuntimeException("BCLForgeLib is needed to run this plugin!");
 		}
 
@@ -57,35 +59,56 @@ public class BetterChunkLoader extends JavaPlugin {
 		try {
 			getLogger().info("Loading config...");
 			this.config = new Config(this);
-			if (DataStoreManager.getDataStore() == null)
+			if (DataStoreManager.getDataStore() == null) {
 				DataStoreManager.setDataStoreInstance(this.config.dataStore);
+			}
 			getLogger().info("Loading " + DataStoreManager.getDataStore().getName() + " Data Store...");
 			DataStoreManager.getDataStore().load();
-			getLogger().info("Loaded " + DataStoreManager.getDataStore().getChunkLoaders().size() + " chunk loaders data.");
-			getLogger().info("Loaded " + DataStoreManager.getDataStore().getPlayersData().size() + " players data.");
-			int count = 0;
+
+			getLogger().info("Loaded " + DataStoreManager.getDataStore().getChunkLoaders().size() + " chunk loaders data from primary datastore.");
+			getLogger().info("Loaded " + DataStoreManager.getDataStore().getPlayersData().size() + " players data from primary datastore.");
+
+			getLogger().info("Synchronizing all chunk loaders with BCLForgeLib...");
+			int activatedCount = 0;
+			int deactivatedCount = 0;
 			for (CChunkLoader cl : DataStoreManager.getDataStore().getChunkLoaders()) {
 				if (cl.isLoadable()) {
 					BCLForgeLib.instance().addChunkLoader(cl);
-					count++;
+					activatedCount++;
+				} else {
+					BCLForgeLib.instance().removeChunkLoader(cl);
+					deactivatedCount++;
 				}
 			}
-			getLogger().info("Loaded " + count + " always-on chunk loaders.");
+			getLogger().info("Synchronization complete: Activated " + activatedCount + " chunk loaders, ensured " + deactivatedCount + " non-loadable ones are deactivated in BCLForgeLib.");
+
 			getLogger().info("Loading Listeners...");
-			getServer().getPluginManager().registerEvents(new EventListener(this), (Plugin)this);
+			getServer().getPluginManager().registerEvents(new EventListener(this), this);
 			getCommand("betterchunkloader").setExecutor(new CommandExec(this));
 			getLogger().info("Load complete.");
 		} catch (Exception e) {
 			e.printStackTrace();
-			getLogger().warning("Load failed!");
-			Bukkit.getPluginManager().disablePlugin((Plugin)this);
+			getLogger().severe("Load failed! Disabling plugin.");
+			Bukkit.getPluginManager().disablePlugin(this);
 		}
 	}
 
+	@Override
 	public void onDisable() {
-		for (CChunkLoader cl : DataStoreManager.getDataStore().getChunkLoaders())
-			BCLForgeLib.instance().removeChunkLoader(cl);
+		getLogger().info("Disabling BetterChunkLoader...");
+		if (DataStoreManager.getDataStore() != null && DataStoreManager.getDataStore().getChunkLoaders() != null) {
+			getLogger().info("Removing all chunk loaders from BCLForgeLib...");
+			for (CChunkLoader cl : DataStoreManager.getDataStore().getChunkLoaders()) {
+				if (BCLForgeLib.instance() != null) {
+					BCLForgeLib.instance().removeChunkLoader(cl);
+				}
+			}
+			getLogger().info("All chunk loaders removed from BCLForgeLib.");
+		} else {
+			getLogger().warning("DataStore not available or no chunk loaders to remove during disable.");
+		}
 		instance = null;
+		getLogger().info("BetterChunkLoader disabled.");
 	}
 
 	public static BetterChunkLoader instance() {
@@ -94,17 +117,27 @@ public class BetterChunkLoader extends JavaPlugin {
 
 	public static long getPlayerLastPlayed(UUID playerId) {
 		OfflinePlayer player = Bukkit.getOfflinePlayer(playerId);
-		if (player.getLastPlayed() != 0L)
+		if (player.getLastPlayed() != 0L) {
 			return player.getLastPlayed();
-		if (player.getName() != null && !player.getName().isEmpty())
+		}
+		if (player.getName() != null && !player.getName().isEmpty()) {
 			return getPlayerDataLastModified(playerId);
+		}
 		return 0L;
 	}
 
 	public static long getPlayerDataLastModified(UUID playerId) {
-		File playerData = new File(((World)Bukkit.getWorlds().get(0)).getWorldFolder(), "playerdata" + File.separator + playerId.toString() + ".dat");
-		if (playerData.exists())
-			return playerData.lastModified();
+		if (Bukkit.getWorlds().isEmpty()) {
+			BetterChunkLoader.instance().getLogger().warning("Cannot get player data last modified: No worlds loaded.");
+			return 0L;
+		}
+		World world = Bukkit.getWorlds().get(0);
+		File worldFolder = world.getWorldFolder();
+
+		File playerDataFile = new File(worldFolder, "playerdata" + File.separator + playerId.toString() + ".dat");
+		if (playerDataFile.exists()) {
+			return playerDataFile.lastModified();
+		}
 		return 0L;
 	}
 
@@ -112,4 +145,3 @@ public class BetterChunkLoader extends JavaPlugin {
 		return this.config;
 	}
 }
-
--- src/main/java/eu/avalanche7/EventListener.java
+++ src/main/java/eu/avalanche7/EventListener.java
@@ -5,11 +5,11 @@ import java.util.List;
 import java.util.UUID;
 import eu.avalanche7.datastore.DataStoreManager;
 import net.kaikk.mc.bcl.forgelib.BCLForgeLib;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -20,22 +20,18 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.world.WorldLoadEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class EventListener implements Listener {
-	private BetterChunkLoader instance;
+	private BetterChunkLoader pluginInstance;
 
 	private int alwaysOnBlockId;
-
 	private int alwaysOnBlockData;
-
 	private int onlineOnlyBlockId;
-
 	private int onlineOnlyBlockData;
 
 	EventListener(BetterChunkLoader instance) {
-		this.instance = instance;
+		this.pluginInstance = instance;
 		this.alwaysOnBlockId = instance.getConfig().getInt("alwaysOnBlockId", 57);
 		this.alwaysOnBlockData = instance.getConfig().getInt("alwaysOnBlockData", 0);
 		this.onlineOnlyBlockId = instance.getConfig().getInt("onlineOnlyBlockId", 42);
@@ -61,22 +57,26 @@ public class EventListener implements Listener {
 					}
 				} else if (canBreak(clickedBlock, player)) {
 					UUID uid = player.getUniqueId();
+					boolean isCreatingAlwaysOn = false;
 					if (clickedBlock.getTypeId() == this.alwaysOnBlockId && clickedBlock.getData() == this.alwaysOnBlockData) {
 						if (!player.hasPermission(PermissionNode.CHUNK_ALWAYSON)) {
 							player.sendMessage(ChatColor.RED + "You don't have the permission to create always-on chunk loaders." + (player.isOp() ? " (betterchunkloader.alwayson is needed)" : ""));
 							return;
 						}
-						if (player.isSneaking() && player.hasPermission(PermissionNode.ADMIN_LOADER))
+						if (player.isSneaking() && player.hasPermission(PermissionNode.ADMIN_LOADER)) {
 							uid = CChunkLoader.adminUUID;
+						}
+						isCreatingAlwaysOn = true;
 					} else if (clickedBlock.getTypeId() == this.onlineOnlyBlockId && clickedBlock.getData() == this.onlineOnlyBlockData) {
 						if (!player.hasPermission(PermissionNode.CHUNK_ONLINEONLY)) {
 							player.sendMessage(ChatColor.RED + "You don't have the permission to create online-only chunk loaders." + (player.isOp() ? " (betterchunkloader.onlineonly is needed)" : ""));
 							return;
 						}
+						isCreatingAlwaysOn = false;
 					} else {
 						return;
 					}
-					chunkLoader = new CChunkLoader((int)Math.floor(clickedBlock.getX() / 16.0D), (int)Math.floor(clickedBlock.getZ() / 16.0D), clickedBlock.getWorld().getName(), (byte)-1, uid, new BlockLocation(clickedBlock), null, (clickedBlock.getType() == Material.DIAMOND_BLOCK));
+					chunkLoader = new CChunkLoader((int)Math.floor(clickedBlock.getX() / 16.0D), (int)Math.floor(clickedBlock.getZ() / 16.0D), clickedBlock.getWorld().getName(), (byte)-1, uid, new BlockLocation(clickedBlock), null, isCreatingAlwaysOn);
 					chunkLoader.showUI(player);
 				} else {
 					player.sendMessage(ChatColor.RED + "You haven't build permission at this location");
@@ -87,7 +87,7 @@ public class EventListener implements Listener {
 					chunkLoader.showCorners(player);
 				}
 			} else {
-				player.sendMessage(ChatColor.GOLD + "This Blocks can be converted into chunk loaders. Right click it with a blaze rod.");
+				player.sendMessage(ChatColor.GOLD + "This Block can be converted into a chunk loader. Right click it with a blaze rod.");
 			}
 		}
 	}
@@ -95,39 +95,88 @@ public class EventListener implements Listener {
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
 	void onBlockBreak(BlockBreakEvent event) {
 		Block block = event.getBlock();
-		if (block == null || (block.getTypeId() != this.alwaysOnBlockId && block.getTypeId() != this.onlineOnlyBlockId))
-			return;
-		if (block.getData() != this.alwaysOnBlockData && block.getData() != this.onlineOnlyBlockData)
-			return;
+		if (block == null) return;
+
+		boolean isChunkLoaderBlockType = (block.getTypeId() == this.alwaysOnBlockId && block.getData() == this.alwaysOnBlockData) ||
+				(block.getTypeId() == this.onlineOnlyBlockId && block.getData() == this.onlineOnlyBlockData);
+		if (!isChunkLoaderBlockType) return;
+
 		CChunkLoader chunkLoader = DataStoreManager.getDataStore().getChunkLoaderAt(new BlockLocation(block.getLocation()));
-		if (chunkLoader == null)
-			return;
-		DataStoreManager.getDataStore().removeChunkLoader(chunkLoader);
+		if (chunkLoader == null) return;
+
 		Player player = event.getPlayer();
+		if (!player.getUniqueId().equals(chunkLoader.getOwner()) &&
+				!player.hasPermission(PermissionNode.ADMIN_EDIT_ALL) &&
+				!(chunkLoader.isAdminChunkLoader() && player.hasPermission(PermissionNode.ADMIN_LOADER))) {
+			player.sendMessage(ChatColor.RED + "You don't have permission to break this chunk loader.");
+			event.setCancelled(true);
+			return;
+		}
+
+		DataStoreManager.getDataStore().removeChunkLoader(chunkLoader);
+
 		player.sendMessage(ChatColor.RED + "Chunk loader removed.");
-		Player owner = chunkLoader.getPlayer();
-		if (owner != null && player != owner)
-			owner.sendMessage(ChatColor.RED + "Your chunk loader at " + chunkLoader.getLoc().toString() + " has been removed by " + player.getDisplayName() + ".");
-		BetterChunkLoader.instance().getLogger().info(player.getName() + " broke " + chunkLoader.getOwnerName() + "'s chunk loader at " + chunkLoader.getLocationString());
+		Player ownerOnline = chunkLoader.getPlayer();
+		String ownerName = chunkLoader.getOwnerName();
+
+		if (ownerOnline != null && !ownerOnline.equals(player)) {
+			ownerOnline.sendMessage(ChatColor.RED + "Your chunk loader at " + chunkLoader.getLoc().toString() + " has been removed by " + player.getDisplayName() + ".");
+		} else if (ownerOnline == null && !chunkLoader.isAdminChunkLoader() && ownerName != null && !ownerName.isEmpty() && !ownerName.equalsIgnoreCase("Admin")) {
+			if (Bukkit.getPluginManager().isPluginEnabled("Essentials")) {
+				String mailMessage = "Your chunk loader at " + chunkLoader.getLoc().toString() + " was broken by " + player.getDisplayName() + ".";
+				String mailCommand = "mail send " + ownerName + " " + mailMessage;
+				this.pluginInstance.getLogger().info("Attempting to send Essentials mail to offline owner " + ownerName + ": " + mailCommand);
+				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), mailCommand);
+			} else {
+				this.pluginInstance.getLogger().info("Essentials plugin not found. Cannot send mail to offline owner " + ownerName + " for broken chunk loader.");
+			}
+		}
+		this.pluginInstance.getLogger().info(player.getName() + " broke " + chunkLoader.getOwnerName() + "'s chunk loader at " + chunkLoader.getLocationString());
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	void onPlayerLogin(PlayerLoginEvent event) {
-		if (event.getResult() != PlayerLoginEvent.Result.ALLOWED)
-			return;
-		List<CChunkLoader> clList = DataStoreManager.getDataStore().getChunkLoaders(event.getPlayer().getUniqueId());
-		for (CChunkLoader chunkLoader : clList) {
-			if (!chunkLoader.isAlwaysOn() && chunkLoader.blockCheck())
-				BCLForgeLib.instance().addChunkLoader(chunkLoader);
-		}
+		if (event.getResult() != PlayerLoginEvent.Result.ALLOWED) return;
+
+		final UUID playerUUID = event.getPlayer().getUniqueId();
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				Player player = pluginInstance.getServer().getPlayer(playerUUID);
+				if (player == null || !player.isOnline()) return;
+
+				pluginInstance.getLogger().info("Player " + player.getName() + " logged in. Checking online-only chunk loaders.");
+				List<CChunkLoader> clList = DataStoreManager.getDataStore().getChunkLoaders(playerUUID);
+				int activatedCount = 0;
+				for (CChunkLoader chunkLoader : clList) {
+					if (!chunkLoader.isAlwaysOn() && chunkLoader.blockCheck()) {
+						if (chunkLoader.isLoadable()) {
+							BCLForgeLib.instance().addChunkLoader(chunkLoader);
+							activatedCount++;
+						}
+					}
+				}
+				if (activatedCount > 0) {
+					pluginInstance.getLogger().info("Activated " + activatedCount + " online-only chunk loaders for " + player.getName());
+				}
+			}
+		}.runTaskLater(this.pluginInstance, 20L);
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	void onPlayerQuit(PlayerQuitEvent event) {
-		List<CChunkLoader> clList = DataStoreManager.getDataStore().getChunkLoaders(event.getPlayer().getUniqueId());
+		Player player = event.getPlayer();
+		pluginInstance.getLogger().info("Player " + player.getName() + " logged out. Deactivating their online-only chunk loaders.");
+		List<CChunkLoader> clList = DataStoreManager.getDataStore().getChunkLoaders(player.getUniqueId());
+		int deactivatedCount = 0;
 		for (CChunkLoader chunkLoader : clList) {
-			if (!chunkLoader.isAlwaysOn())
+			if (!chunkLoader.isAlwaysOn()) {
 				BCLForgeLib.instance().removeChunkLoader(chunkLoader);
+				deactivatedCount++;
+			}
+		}
+		if (deactivatedCount > 0) {
+			pluginInstance.getLogger().info("Deactivated " + deactivatedCount + " online-only chunk loaders for " + player.getName());
 		}
 	}
 
@@ -137,27 +186,57 @@ public class EventListener implements Listener {
 			Player player = (Player)event.getWhoClicked();
 			event.setCancelled(true);
 			CChunkLoader chunkLoader = (CChunkLoader)event.getInventory().getHolder();
-			if (chunkLoader == null)
-				return;
+			if (chunkLoader == null) return;
+
 			if (chunkLoader.isAdminChunkLoader()) {
 				if (!player.hasPermission(PermissionNode.ADMIN_LOADER)) {
 					player.sendMessage(ChatColor.RED + "You don't have permissions for this!");
+					closeInventory(player);
 					return;
 				}
 			} else if (!player.getUniqueId().equals(chunkLoader.getOwner()) && !player.hasPermission(PermissionNode.ADMIN_EDIT_ALL)) {
 				player.sendMessage(ChatColor.RED + "You can't edit others' chunk loaders.");
+				closeInventory(player);
 				return;
 			}
+
 			byte pos = (byte)event.getRawSlot();
 			if (chunkLoader.getRange() != -1) {
 				if (pos == 0) {
 					DataStoreManager.getDataStore().removeChunkLoader(chunkLoader);
+					player.sendMessage(ChatColor.GREEN + "Chunk loader removed.");
 					closeInventory(player);
-				} else if (pos > 1 && pos < 7) {
-					pos = (byte)(pos - 2);
-					if (!chunkLoader.isAdminChunkLoader() && !player.hasPermission(PermissionNode.CHUNKS_UNLIMITED) &&
-							pos > chunkLoader.getRange()) {
-						int available, needed = (1 + pos * 2) * (1 + pos * 2) - chunkLoader.size();
+				} else if (pos >= 2 && pos <= 6) {
+					byte newRange = (byte)(pos - 2);
+					if (!chunkLoader.isAdminChunkLoader() && !player.hasPermission(PermissionNode.CHUNKS_UNLIMITED)) {
+						int currentSize = chunkLoader.size();
+						int newSize = (1 + newRange * 2) * (1 + newRange * 2);
+						int needed = newSize - currentSize;
+
+						int available;
+						if (chunkLoader.isAlwaysOn()) {
+							available = DataStoreManager.getDataStore().getAlwaysOnFreeChunksAmount(chunkLoader.getOwner());
+						} else {
+							available = DataStoreManager.getDataStore().getOnlineOnlyFreeChunksAmount(chunkLoader.getOwner());
+						}
+
+						if (needed > 0 && needed > available) {
+							player.sendMessage(ChatColor.RED + "Not enough free chunks! Needed: " + needed + ". Available: " + available + ".");
+							closeInventory(player);
+							return;
+						}
+					}
+					this.pluginInstance.getLogger().info(player.getName() + " edited " + chunkLoader.getOwnerName() + "'s chunk loader at " + chunkLoader.getLocationString() + " range from " + chunkLoader.getRange() + " to " + newRange);
+					DataStoreManager.getDataStore().changeChunkLoaderRange(chunkLoader, newRange);
+					player.sendMessage(ChatColor.GOLD + "Chunk Loader updated to size " + chunkLoader.sizeX() + ".");
+					closeInventory(player);
+				}
+			} else {
+				if (pos >= 2 && pos <= 6) {
+					byte newRange = (byte)(pos - 2);
+					if (!chunkLoader.isAdminChunkLoader() && !player.hasPermission(PermissionNode.CHUNKS_UNLIMITED)) {
+						int needed = (1 + newRange * 2) * (1 + newRange * 2);
+						int available;
 						if (chunkLoader.isAlwaysOn()) {
 							available = DataStoreManager.getDataStore().getAlwaysOnFreeChunksAmount(chunkLoader.getOwner());
 						} else {
@@ -169,55 +248,50 @@ public class EventListener implements Listener {
 							return;
 						}
 					}
-					BetterChunkLoader.instance().getLogger().info(player.getName() + " edited " + chunkLoader.getOwnerName() + "'s chunk loader at " + chunkLoader.getLocationString() + " range from " + chunkLoader.getRange() + " to " + pos);
-					DataStoreManager.getDataStore().changeChunkLoaderRange(chunkLoader, pos);
-					player.sendMessage(ChatColor.GOLD + "Chunk Loader updated.");
+					chunkLoader.setRange(newRange);
+					chunkLoader.setCreationDate(new Date());
+					this.pluginInstance.getLogger().info(player.getName() + " made a new " + (chunkLoader.isAdminChunkLoader() ? "admin " : "") + "chunk loader at " + chunkLoader.getLocationString() + " with range " + newRange);
+					DataStoreManager.getDataStore().addChunkLoader(chunkLoader);
+					player.sendMessage(ChatColor.GOLD + "Chunk Loader created with size " + chunkLoader.sizeX() + ".");
 					closeInventory(player);
 				}
-			} else if (pos > 1 && pos < 7) {
-				pos = (byte)(pos - 2);
-				if (!chunkLoader.isAdminChunkLoader() && !player.hasPermission(PermissionNode.CHUNKS_UNLIMITED)) {
-					int available, needed = (1 + pos * 2) * (1 + pos * 2);
-					if (chunkLoader.isAlwaysOn()) {
-						available = DataStoreManager.getDataStore().getAlwaysOnFreeChunksAmount(chunkLoader.getOwner());
-					} else {
-						available = DataStoreManager.getDataStore().getOnlineOnlyFreeChunksAmount(chunkLoader.getOwner());
-					}
-					if (needed > available) {
-						player.sendMessage(ChatColor.RED + "Not enough free chunks! Needed: " + needed + ". Available: " + available + ".");
-						closeInventory(player);
-						return;
-					}
-				}
-				chunkLoader.setRange(pos);
-				chunkLoader.setCreationDate(new Date());
-				BetterChunkLoader.instance().getLogger().info(player.getName() + " made a new " + (chunkLoader.isAdminChunkLoader() ? "admin " : "") + "chunk loader at " + chunkLoader.getLocationString() + " with range " + pos);
-				DataStoreManager.getDataStore().addChunkLoader(chunkLoader);
-				closeInventory(player);
-				player.sendMessage(ChatColor.GOLD + "Chunk Loader created.");
 			}
 		}
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	void onWorldLoad(WorldLoadEvent event) {
-		for (CChunkLoader cl : DataStoreManager.getDataStore().getChunkLoaders(event.getWorld().getName())) {
-			if (cl.isLoadable())
-				BCLForgeLib.instance().addChunkLoader(cl);
+		this.pluginInstance.getLogger().info("Bukkit WorldLoadEvent for: " + event.getWorld().getName() + ". Synchronizing its chunk loaders with BCLForgeLib.");
+		int activatedInWorld = 0;
+		int deactivatedInWorld = 0;
+		if (DataStoreManager.getDataStore() == null) {
+			this.pluginInstance.getLogger().warning("DataStore not available during WorldLoadEvent for " + event.getWorld().getName());
+			return;
 		}
+
+		for (CChunkLoader cl : DataStoreManager.getDataStore().getChunkLoaders(event.getWorld().getName())) {
+			if (cl.isLoadable()) {
+				BCLForgeLib.instance().addChunkLoader(cl);
+				activatedInWorld++;
+			} else {
+				BCLForgeLib.instance().removeChunkLoader(cl);
+				deactivatedInWorld++;
+			}
+		}
+		this.pluginInstance.getLogger().info("For world " + event.getWorld().getName() + ": Activated " + activatedInWorld + ", ensured deactivation for " + deactivatedInWorld + " via BCLForgeLib.");
 	}
 
 	private static void closeInventory(final Player p) {
-		(new BukkitRunnable() {
+		new BukkitRunnable() {
 			public void run() {
 				p.closeInventory();
 			}
-		}).runTaskLater((Plugin)BetterChunkLoader.instance(), 1L);
+		}.runTaskLater(BetterChunkLoader.instance(), 1L);
 	}
 
 	static boolean canBreak(Block block, Player player) {
 		BlockBreakEvent bbe = new BlockBreakEvent(block, player);
-		BetterChunkLoader.instance().getServer().getPluginManager().callEvent((Event)bbe);
+		BetterChunkLoader.instance().getServer().getPluginManager().callEvent(bbe);
 		return !bbe.isCancelled();
 	}
 }
